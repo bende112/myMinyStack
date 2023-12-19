8ec93f1c109fb3d78d931c059eeb81f68d99c8d7
Bende <bende.nku@yahoo.com>
2023-12-19 10:46:31 +0000

    full stack app

---
+++ client/app.js
@@ -0,0 +1,13 @@
+async function getMessage() {
+  // call our API with fetch
+  const response = await fetch("http://localhost:8080/message");
+
+  // turn the response into json
+  const json = await response.json();
+  console.log(json);
+  // put my message on the page
+  const p = document.getElementById("message");
+  p.textContent = json.message;
+}
+getMessage();
+
